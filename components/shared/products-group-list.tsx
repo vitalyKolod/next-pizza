60d8c279b97b1312a694b3425a362a07581e98c4
Vitaly Kolodchenko <kolodchenkovitaly250@gmail.com>
2026-02-15 01:33:06 +0300

    fix scroll

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -18,24 +18,24 @@ interface Props {
 export const ProductsGroupList: React.FC<Props> = ({
   title,
   items,
-  // categoryId,
+  categoryId,
   className,
   listClassName,
 }) => {
-  // const setActiveCategoryId = useCategoryStore((state) => state.setActiveId)
-  // const intersectionRef = React.useRef(null)
-  // const intersection = useIntersection(intersectionRef, {
-  //   threshold: 0.4,
-  // })
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId)
+  const intersectionRef = React.useRef(null)
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  })
 
-  // React.useEffect(() => {
-  //   if (intersection?.isIntersecting) {
-  //     setActiveCategoryId(categoryId)
-  //   }
-  // }, [categoryId, intersection?.isIntersecting, title])
+  React.useEffect(() => {
+    if (intersection?.isIntersecting) {
+      setActiveCategoryId(categoryId)
+    }
+  }, [categoryId, intersection?.isIntersecting, title])
 
   return (
-    <div className={className} id={title}>
+    <div className={className} id={title} ref={intersectionRef}>
       <Title text={title} size="lg" className="font-extrabold mb-5" />
       <div className={cn('grid grid-cols-3 gap-12.5', listClassName)}>
         {items.map((product, i) => (
